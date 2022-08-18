9c65fea084186c1d1241887fcaa18def86b7689e
alexthorrold <91945780+alexthorrold@users.noreply.github.com>
2022-08-19 06:48:01 +1200

    Added onClick functionality to each of the four icons in the lower half of the page. Current functionality is to create a Toast.

--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.assignmentone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickComputerScience(View view) {
+        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickDesign(View view) {
+        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickMath(View view) {
+        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickSoftwareEngineering(View view) {
+        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
     }
 }
